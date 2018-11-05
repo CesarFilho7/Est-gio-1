b4e85b55c1c612ebdc6e4d46322aca2d725b0ced
Cesar Filho <cesarfec7@gmail.com>
2018-11-05 10:40:11 -0300

    alterãcoes no galho

--- main.lua
+++ main.lua
@@ -6,52 +6,84 @@
 
 -- Your code here
 
+local galhoTable = {}
+local collisionFilter1 = { groupIndex = -1 }
 
 local physics = require("physics")
 physics.start()
---physics.setDrawMode( "hybrid" )
---physics.setGravity( 0, 0 )
-
+physics.setDrawMode( "hybrid" )
 display.setStatusBar( display.HiddenStatusBar )
 system.activate( "multitouch" )
 
-local Height = 0
+local musica = audio.loadStream( "musica.mp3")
+--audio.play(musica)
 
---plataforma da esquerda
-local plataforma1 = display.newRect(display.contentWidth - 287, display.contentHeight - 115, 25,15)
-physics.addBody(plataforma1, "static", {density=3.0, friction=0.5, bounce=0})
+--plataforma invisivel da esquerda
+local plataforma1 = display.newRect(display.contentWidth - 287, display.contentHeight - 100, 50,15)
+physics.addBody(plataforma1, "static", {density=3.0, friction=0.5, bounce=0, filter = collisionFilter1})
+--plataforma invisivel da direita
+local plataforma2 = display.newRect( display.contentWidth - 32, display.contentHeight - 100, 50, 15) --(display.contentWidth - 32) posição x, depois posição y, largura do objeto, altura do objeto 
+physics.addBody(plataforma2, "static", {density=3.0, friction=0.5, bounce=0, filter = collisionFilter1})
 
---plataforma da direita
-local plataforma2 = display.newRect( display.contentWidth - 32, display.contentHeight - 115, 25, 15) --(display.contentWidth - 32) posição x, depois posição y, largura do objeto, altura do objeto 
-physics.addBody(plataforma2, "static", {density=3.0, friction=0.5, bounce=0})
-
-local background = display.newImageRect( "background.png", 360, 570 )
+-- criação do background
+local background = display.newImageRect( "background.png", 360, 670 )
 background.x = display.contentCenterX
 background.y = display.contentCenterY
 
-local contadorAltura = display.newText( Height, display.contentCenterX, 22, native.systemFont, 30 )
-contadorAltura:setFillColor( 0, 0, 0 )
-
+-- Parede invisivel da Esquerda
 local paredeLateralEsquerda = display.newRect(0, 240, 40, display.contentHeight )
-physics.addBody(paredeLateralEsquerda, "static", ({density=3.0, friction=0.5, bounce=0}))
-
+physics.addBody(paredeLateralEsquerda, "static", ({density=3.0, friction=0.5, bounce=0, filter =  collisionFilter1}))
+-- Parede invisivel da Direita
 local paredeLateralDireita = display.newRect(320, 240, 40, display.contentHeight )
-physics.addBody(paredeLateralDireita, "static", ({density=3.0, friction=0.5, bounce=0}))
+physics.addBody(paredeLateralDireita, "static", ({density=3.0, friction=0.5, bounce=0, filter = collisionFilter1}))
 
-local arvore = display.newImageRect( "tree.png", 360, 570)
-arvore.x = display.contentCenterX
-arvore.y = display.contentCenterY
+-- Duplicando a imagem da árvora para poder fazer a animação no cenário
+local arvore = display.newImage( "tree2.png")
+arvore.x = 160
+arvore.y = display.contentHeight / 2
+arvore.xScale = 0.47
+arvore.yScale = 1
 
--- local arvore2 = display.newImageRect( "tree2.png" 360, 570 )
+local arvore2 = display.newImage( "tree2.png" )
+arvore2.x = 160
+arvore2.y =  0
+arvore2.xScale = 0.47
+arvore2.yScale = 1
 
+-- Junção das imagens 
+local contador = 0 
+function move(event)
+	contador =  5
+	arvore.y = arvore.y + contador
+	arvore2.y = arvore2.y + contador
+	
+	if arvore.y > display.contentHeight then
+		arvore.y = 0
+	end
+	if (arvore2.y >  display.contentHeight) then
+		arvore2.y = 0
+	end
+end
 
+-- Criando o número que vai ser a pontuação
+local contadorAltura = display.newText( "0", display.contentCenterX, 10, native.systemFont, 30 )
+contadorAltura:setFillColor( black )
 
+local pastTime = 000  -- 10 minutes * 60 seconds
 
--- configurando altura, largura do sprite e o num de frames
+-- Função que conta o tempo (Pontuação)
+function updateTime( event ) 
+    pastTime = pastTime + 1
+    contadorAltura.text = pastTime
+end
+
+local countDownTimer = timer.performWithDelay( 600, updateTime, pastTime )
+
+-- Configurando altura, largura do sprite e o número de frames
 local sheetOptions  = {width = 100, height = 100, numFrames = 10}
 local sheet = graphics.newImageSheet( "spritecesar4.png", sheetOptions )
 
---definindo a animação 
+-- Definindo a animação 
 local sequences = {
 	{
 		name = "RunRight",
@@ -68,17 +100,15 @@ local sequences = {
 	}
 }
 
-
-
 local player = display.newSprite( sheet, sequences )
 
 -- definindo a posição do meu personagem
 player.x = 281
-player.y = 345
+player.y = 363
 player.width =20
 player.height =30
-player.xScale = 1.2
-player.yScale = 1.2
+player.xScale = 1.1
+player.yScale = 1.1
 player: rotate(270)
 player:play()
 
@@ -87,22 +117,26 @@ physics.addBody( player, "dynamic", {density=1.53,bounce=0, friction= 0} )
 local direita = true
 function touchAction( event )
  	-- indica que um toque começou na tela.
-    if ( event.phase == "began" ) then
+    if ( event.phase == "began" and (player.x >= 279 or player.x <= 36)) then
     	local vx, vy = player:getLinearVelocity()
-        player:setLinearVelocity( vx, 0 )
+        player:setLinearVelocity( 0, vy )
         
 
         if (direita == true) then
         	--altura e direção do pulo
-        	player:applyLinearImpulse( -6, -7, player.x, player.y )
+        	local jump = audio.loadStream( "jump.mp3")
+			--audio.play(jump)
+        	player:applyLinearImpulse( -18, -2.5, player.x, player.y )
         	direita = false
        		player:setSequence( "RunLeft" )
         	player:play()
         	player:rotate(180)	
        	else if (direita == false) then
+        	local jump = audio.loadStream( "jump.mp3")
+			--audio.play(jump)
        		direita = true
        		player:setSequence( "RunRight" )
-        	player:applyLinearImpulse( 6, -7, player.x, player.y )
+        	player:applyLinearImpulse( 18, -2.5, player.x, player.y )
        		player:play()
        		player:rotate(180)
        		end
@@ -110,6 +144,33 @@ function touchAction( event )
 	end
 	return true
 end
-Runtime:addEventListener( "touch", touchAction )
+
+function criarGalho(event)
+	
+   	local whereFrom = math.random(2)
+
+	    if ( whereFrom == 1 ) then
+	    	local novoGalho = display.newImageRect("galho1.png", 110, 70)
+	    	physics.addBody( novoGalho, "dynamic", {density = -2, friction = 0, bounce = 0, filter = collisionFilter1})
+		   	table.insert( galhoTable, novoGalho)
+			novoGalho.myName = "galho"
+			novoGalho.x = display.contentCenterX + 110
+			novoGalho.y = -100
+			novoGalho:setLinearVelocity(0, -100)
+		else
+		 	local novoGalho = display.newImageRect("galho2.png", 90, 70)
+		 	physics.addBody( novoGalho, "dynamic", {density = -2, friction = 0, bounce = 0, filter = collisionFilter1})
+		   	table.insert( galhoTable, novoGalho)
+			novoGalho.myName = "galho"
+			novoGalho.x = display.contentCenterX - 110
+			novoGalho.y = -100
+			novoGalho:setLinearVelocity(0, -100)
+		end
+end
 
 
+-- Cria um galho a cada 1s = 1000
+local geradorDeGalho = timer.performWithDelay( 700, criarGalho, pastTime)
+
+Runtime:addEventListener( "touch", touchAction )	
+Runtime:addEventListener( "enterFrame", move);
